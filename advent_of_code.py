5963e6bb81c3fb662c04cb6c0128fcc690722ed9
Markus Mertama <markus.mertama@intopalo.com>
2020-12-05 10:41:37 +0200

    day 5

--- advent_of_code.py
+++ advent_of_code.py
@@ -212,17 +212,12 @@ def check_passports(data, keys):
 
     valids = 0
     for record in record_data:
-        print(record, end=":")
         is_valid = functools.reduce(lambda c, k: c
                                                  and k in record.keys()
                                                  and (isinstance(keys, (set, str))
                                                  or keys[k](record[k])), keys, True)
         if is_valid:
             valids += 1
-            print("valid")
-        else:
-            print("rejected")
-
     return valids
 
 
@@ -256,27 +251,62 @@ def check_passports_loose(data):
             'pid'}  # no cid :-D
     valids = check_passports(data, keys)
     print("Valid passports", valids)
+
+
 #####
+
+example5 = '''FBFBBFFRLR
+BFFFBBFRRR
+FFFBBBFRRR
+BBFFBBFRLL'''
+
+
+def find_seat(data):
+    max_id = 0
+    min_id = 100000
+    occupied_seats = set()
+    for line in data:
+        row_data = line[:7]
+        col_data = line[7:]
+        row = 0
+        for r in row_data:
+            row <<= 1
+            row |= 0 if r == 'F' else 1
+        col = 0
+        for c in col_data:
+            col <<= 1
+            col |= 0 if c == 'L' else 1
+        seat_id = row * 8 + col
+        if seat_id < min_id:
+            min_id = seat_id
+        if seat_id > max_id:
+            max_id = seat_id
+        occupied_seats.add(seat_id)
+    print("max seat id:", max_id)
+
+    for s in range(min_id, max_id + 1):
+        if s not in occupied_seats:
+            print(s, "missing")
 
 
 if __name__ == "__main__":
-    tasks = {'1_1': find_two_factors,
-             '1_2': find_three_factors}
     # find_two_factors(read_local_lines(example1))
-    # find_two_factors(read_remote_lines('data\\input.txt'))
+    find_two_factors(read_remote_lines('data/input.txt'))
     # find_three_factors(read_local_lines(example1))
-    # find_three_factors(read_remote_lines('data\\input.txt'))
+    find_three_factors(read_remote_lines('data/input.txt'))
     # validate_passwords(read_local_lines(example2))
-    # validate_passwords(read_remote_lines('data\\input2.txt'))
+    validate_passwords(read_remote_lines('data/input2.txt'))
     # validate_passwords_2(read_local_lines(example2))
-    # validate_passwords_2(read_remote_lines('data\\input2.txt'))
+    validate_passwords_2(read_remote_lines('data/input2.txt'))
     # navigate_thru_trees(3, 1, read_local_lines(example3))
-    # navigate_thru_trees(3, 1, read_remote_lines('data\\input3.txt'))
+    navigate_thru_trees(3, 1, read_remote_lines('data/input3.txt'))
     # navigate_thru_trees2(read_local_lines(example3))
-    # navigate_thru_trees2(read_remote_lines('data\\input3.txt'))
+    navigate_thru_trees2(read_remote_lines('data/input3.txt'))
     # check_passports_loose(read_local_lines(example4))
-    # check_passports_loose(read_remote_lines('data\\input4.txt'))
-    #check_passports_strict(read_local_lines(example4))
-    #check_passports_strict(read_local_lines(example4_invalids))
-    #check_passports_strict(read_local_lines(example4_valids))
-    check_passports_strict(read_remote_lines('data\\input4.txt'))
+    check_passports_loose(read_remote_lines('data/input4.txt'))
+    # check_passports_strict(read_local_lines(example4))
+    # check_passports_strict(read_local_lines(example4_invalids))
+    # check_passports_strict(read_local_lines(example4_valids))
+    check_passports_strict(read_remote_lines('data/input4.txt'))
+    # find_seat(read_local_lines(example5))
+    find_seat(read_remote_lines('data/input5.txt'))
