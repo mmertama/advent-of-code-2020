40c1269b3e5a4a45cbdfa9f9b98e389ba130b829
Markus Mertama <markus.mertama@intopalo.com>
2020-12-14 01:00:55 +0200

    add test

--- day13.py
+++ day13.py
@@ -43,29 +43,55 @@ def find_timestamp_dummy(data):
 
 
 def find_timestamp(data):
-    ids = [0 if x == 'x' else int(x) for x in data[1].split(',')]
-    #ids = [2, 3, 5]
-    ids_values = [x for x in ids if x > 0]
-    ids_deltas = [ids.index(x) for x in ids if x > 0]
+    #ids = [0 if x == 'x' else int(x) for x in data[1].split(',')]
+    ids = [7, 13, 0, 0, 59, 0, 31, 19]
+    zipped = zip([x for x in ids if x > 0], [ids.index(x) for x in ids if x > 0])
+    id_values = sorted(zipped, reverse=True)
+    #ids_values =
+    #ids_values.sort(reverse=True)
+    #ids_deltas =
+    #ids_deltas.sort(key=ids_values.__getitem__, reverse=True)
     #values = {v: v for v in ids if v != 0}
 
+    id_values.pop(0)
     max_val = max(ids)
     max_val_index = ids.index(max_val)
     t = max_val - max_val_index
-    r = range(0, len(ids_values))
+    r = range(0, len(id_values))
     while True:
-        delta = 0
-        ii = [0] * len(ids_values)
-        for i in r:
-            d = (t + ids_deltas[i]) % ids_values[i]
-            if d > delta:
-                delta = d
+        #delta = 0
+        #ii = [0] * len(ids_values)
+        found = True
+        for i in id_values:
+            if (t + i[1]) % i[0] > 0:
+                found = False
                 break
-            ii[i] = d
-        if delta == 0:
+            #ii[i] = d
+        if found:
             print(t)
             return
         #print(t, delta, ii)
+        t += max_val
+
+
+def find_timestamp_t(data):
+    ids = [0 if x == 'x' else int(x) for x in data[1].split(',')]
+    zipped = zip([x for x in ids if x > 0], [ids.index(x) for x in ids if x > 0])
+    id_values = sorted(zipped, reverse=True)
+    id_values.pop(0)
+    max_val = max(ids)
+    max_val_index = ids.index(max_val)
+    t = max_val - max_val_index
+    r = range(0, len(id_values))
+    while True:
+        found = True
+        for i in id_values:
+            if (t + i[1]) % i[0] > 0:
+                found = False
+                break
+        if found:
+            print(t)
+            return
         t += max_val
 
 
